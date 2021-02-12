8bb49c2c11e5d5931508fb86d6768736a905a909
Nik <15204718+Menonn0228@users.noreply.github.com>
2021-02-12 15:27:03 -0600

    Refactored Example Directory

--- Example/PushDownStateExample/AppDelegate.swift
+++ Example/PushDownStateExample/AppDelegate.swift
@@ -1,15 +1,17 @@
 //
 //  AppDelegate.swift
-//  Example
+//  PushDownStateExample
 //
-//  Created by Nikhil Menon on 2/4/21.
+//  Created by Nikhil Menon on 2/12/21.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -23,5 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
 }
+
